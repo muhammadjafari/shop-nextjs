9eceec85c1e183d1044c027e17d2491791165ff7
Mohammad Jafari <jafariimuhammad@gmail.com>
2024-04-20 10:39:14 +0330

    i18n added

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  i18n: {
+    // providing the locales supported by your application
+   locales: ["en-US", "es-ES", "it-IT"],
+   //  default locale used when the non-locale paths are visited
+   defaultLocale: "en-US",
+ },
 };
 
 export default nextConfig;
